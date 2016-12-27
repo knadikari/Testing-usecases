963f1f5fc6db807ebde05f048f84da4343821338
Kushan Adikari <knadikari@gmail.com>
2016-12-27 11:45:29 +0530

    Update gen.py

--- gen.py
+++ gen.py
@@ -5,14 +5,3 @@ from random import randint
 def gen():
     num = randint(0,9)
     return num
-
-def send():
-    data = {}
-    number = gen()
-    data["num"]= number
-    print number
-    # should send as json.dumps(data)
-
-
-while True:
-    send()
