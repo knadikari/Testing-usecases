7213f7ca899e6d7c6359bda890e394abb04630ef
Kushan Adikari <knadikari@gmail.com>
2016-12-27 11:46:13 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -6,14 +6,3 @@ def sub(data):
     recv_data = json.loads(data)
     number = data["num"] - 10
     return number
-
-
-def send():
-    data = {}
-    number = sub()
-    data["num"] = number
-    print number
-    # should send as json.dumps(data)
-
-while True:
-    send()
