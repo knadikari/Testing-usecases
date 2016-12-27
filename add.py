bb0e468953fd4423e9d8e558c4cd9eafed5193c5
Kushan Adikari <knadikari@gmail.com>
2016-12-27 11:45:01 +0530

    Update add.py

--- add.py
+++ add.py
@@ -6,14 +6,3 @@ def add(data):
     recv_data = json.loads(data)
     number = data["num"] + 10
     return number
-
-
-def send():
-    data = {}
-    number = add()
-    data["num"] = number
-    print number
-    # should send as json.dumps(data)
-
-while True:
-    send()
